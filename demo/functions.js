1a71392acd2cad9e5e6e827eee6230aee228dae7
vip-yy <52729884+yymu@users.noreply.github.com>
2020-07-30 21:01:53 +0800

    Update functions.js

--- demo/functions.js
+++ demo/functions.js
@@ -1,1 +1,323 @@
+//   深拷贝
+function clone (obj) {
+  // 判断是否为空未定义
+  if (null == obj || "object" != typeof obj) return obj;
 
+  //  Date
+  if (obj instanceof Date) {
+      var copy = new Date();
+      copy.setTime(obj.getTime());
+      return copy;
+  }
+
+  //  Array
+  if (obj instanceof Array) {
+      var copy = [];
+      for (var i = 0, len = obj.length; i < len; ++i) {
+          copy[i] = clone(obj[i]);
+      }
+      return copy;
+  }
+
+  // Object
+  if (obj instanceof Object) {
+      var copy = {};
+      for (var attr in obj) {
+          if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+      }
+      return copy;
+  }
+
+  throw new Error("无法复制对象！不支持其类型。");
+}
+
+
+// 随机数时间戳
+function uniqueId() {
+  var a = Math.random,
+    b = parseInt;
+  return (
+    Number(new Date()).toString() + b(10 * a()) + b(10 * a()) + b(10 * a())
+  );
+}
+
+// 时间个性化输出功能
+/*
+1、< 60s, 显示为“刚刚”
+2、>= 1min && < 60 min, 显示与当前时间差“XX分钟前”
+3、>= 60min && < 1day, 显示与当前时间差“今天 XX:XX”
+4、>= 1day && < 1year, 显示日期“XX月XX日 XX:XX”
+5、>= 1year, 显示具体日期“XXXX年XX月XX日 XX:XX”
+*/
+function timeFormat(time) {
+  var date = new Date(time),
+    curDate = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 10,
+    day = date.getDate(),
+    hour = date.getHours(),
+    minute = date.getMinutes(),
+    curYear = curDate.getFullYear(),
+    curHour = curDate.getHours(),
+    timeStr;
+ 
+  if (year < curYear) {
+    timeStr = year + "年" + month + "月" + day + "日 " + hour + ":" + minute;
+  } else {
+    var pastTime = curDate - date,
+      pastH = pastTime / 3600000;
+ 
+    if (pastH > curHour) {
+      timeStr = month + "月" + day + "日 " + hour + ":" + minute;
+    } else if (pastH >= 1) {
+      timeStr = "今天 " + hour + ":" + minute + "分";
+    } else {
+      var pastM = curDate.getMinutes() - minute;
+      if (pastM > 1) {
+        timeStr = pastM + "分钟前";
+      } else {
+        timeStr = "刚刚";
+      }
+    }
+  }
+  return timeStr;
+}
+
+// 延时执行
+// 比如 sleep(1000) 意味着等待1000毫秒，还可从 Promise、Generator、Async/Await 等角度实现。
+// Promise
+const sleep = time => {
+  return new Promise(resolve => setTimeout(resolve, time));
+};
+ 
+sleep(1000).then(() => {
+  console.log(1);
+});
+ 
+ 
+// Generator
+function* sleepGenerator(time) {
+  yield new Promise(function(resolve, reject) {
+    setTimeout(resolve, time);
+  });
+}
+ 
+sleepGenerator(1000)
+  .next()
+  .value.then(() => {
+    console.log(1);
+  });
+ 
+//async
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+ 
+async function output() {
+  let out = await sleep(1000);
+  console.log(1);
+  return out;
+}
+ 
+output();
+
+ //普通方式
+function sleep(callback, time) {
+  if (typeof callback === "function") {
+    setTimeout(callback, time);
+  }
+}
+ 
+function output() {
+  console.log(1);
+}
+ 
+sleep(output, 1000);
+
+
+// 滚动到顶部
+// 使用document.documentElement.scrollTop 或 document.body.scrollTop 获取到顶部的距离，从顶部
+// 滚动一小部分距离。使用window.requestAnimationFrame()来滚动。
+// @example
+// scrollToTop();
+function scrollToTop() {
+  var c = document.documentElement.scrollTop || document.body.scrollTop;
+ 
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
+}
+
+
+// resize的操作
+(function() {
+  var fn = function() {
+    var w = document.documentElement
+        ? document.documentElement.clientWidth
+        : document.body.clientWidth,
+      r = 1255,
+      b = Element.extend(document.body),
+      classname = b.className;
+    if (w < r) {
+      //当窗体的宽度小于1255的时候执行相应的操作
+    } else {
+      //当窗体的宽度大于1255的时候执行相应的操作
+    }
+  };
+  if (window.addEventListener) {
+    window.addEventListener("resize", function() {
+      fn();
+    });
+  } else if (window.attachEvent) {
+    window.attachEvent("onresize", function() {
+      fn();
+    });
+  }
+  fn();
+})();
+
+
+// 替换全部
+String.prototype.replaceAll = function(s1, s2) {
+  return this.replace(new RegExp(s1, "gm"), s2);
+};
+
+
+// HTML 标签转义
+// @param {Array.<DOMString>} templateData 字符串类型的tokens
+// @param {...} ..vals 表达式占位符的运算结果tokens
+//
+function SaferHTML(templateData) {
+  var s = templateData[0];
+  for (var i = 1; i < arguments.length; i++) {
+    var arg = String(arguments[i]);
+    // Escape special characters in the substitution.
+    s += arg
+      .replace(/&/g, "&")
+      .replace(/</g, "<")
+      .replace(/>/g, ">");
+    // Don't escape special characters in the template.
+    s += templateData[i];
+  }
+  return s;
+}
+// 调用
+var html = SaferHTML`<p>这是关于字符串模板的介绍</p>`;
+
+
+// 动态加载脚本文件
+function appendscript(src, text, reload, charset) {
+  var id = hash(src + text);
+  if (!reload && in_array(id, evalscripts)) return;
+  if (reload && $(id)) {
+    $(id).parentNode.removeChild($(id));
+  }
+ 
+  evalscripts.push(id);
+  var scriptNode = document.createElement("script");
+  scriptNode.type = "text/javascript";
+  scriptNode.id = id;
+  scriptNode.charset = charset
+    ? charset
+    : BROWSER.firefox
+    ? document.characterSet
+    : document.charset;
+  try {
+    if (src) {
+      scriptNode.src = src;
+      scriptNode.onloadDone = false;
+      scriptNode.onload = function() {
+        scriptNode.onloadDone = true;
+        JSLOADED[src] = 1;
+      };
+      scriptNode.onreadystatechange = function() {
+        if (
+          (scriptNode.readyState == "loaded" ||
+            scriptNode.readyState == "complete") &&
+          !scriptNode.onloadDone
+        ) {
+          scriptNode.onloadDone = true;
+          JSLOADED[src] = 1;
+        }
+      };
+    } else if (text) {
+      scriptNode.text = text;
+    }
+    document.getElementsByTagName("head")[0].appendChild(scriptNode);
+  } catch (e) {}
+}
+
+
+// 时间日期格式转换
+Date.prototype.format = function(formatStr) {
+  var str = formatStr;
+  var Week = ["日", "一", "二", "三", "四", "五", "六"];
+  str = str.replace(/yyyy|YYYY/, this.getFullYear());
+  str = str.replace(
+    /yy|YY/,
+    this.getYear() % 100 > 9
+      ? (this.getYear() % 100).toString()
+      : "0" + (this.getYear() % 100)
+  );
+  str = str.replace(
+    /MM/,
+    this.getMonth() + 1 > 9
+      ? (this.getMonth() + 1).toString()
+      : "0" + (this.getMonth() + 1)
+  );
+  str = str.replace(/M/g, this.getMonth() + 1);
+  str = str.replace(/w|W/g, Week[this.getDay()]);
+  str = str.replace(
+    /dd|DD/,
+    this.getDate() > 9 ? this.getDate().toString() : "0" + this.getDate()
+  );
+  str = str.replace(/d|D/g, this.getDate());
+  str = str.replace(
+    /hh|HH/,
+    this.getHours() > 9 ? this.getHours().toString() : "0" + this.getHours()
+  );
+  str = str.replace(/h|H/g, this.getHours());
+  str = str.replace(
+    /mm/,
+    this.getMinutes() > 9
+      ? this.getMinutes().toString()
+      : "0" + this.getMinutes()
+  );
+  str = str.replace(/m/g, this.getMinutes());
+  str = str.replace(
+    /ss|SS/,
+    this.getSeconds() > 9
+      ? this.getSeconds().toString()
+      : "0" + this.getSeconds()
+  );
+  str = str.replace(/s|S/g, this.getSeconds());
+  return str;
+};
+ 
+// 或
+Date.prototype.format = function(format) {
+  var o = {
+    "M+": this.getMonth() + 1, //month
+    "d+": this.getDate(), //day
+    "h+": this.getHours(), //hour
+    "m+": this.getMinutes(), //minute
+    "s+": this.getSeconds(), //second
+    "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+    S: this.getMilliseconds() //millisecond
+  };
+  if (/(y+)/.test(format))
+    format = format.replace(
+      RegExp.$1,
+      (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
+  for (var k in o) {
+    if (new RegExp("(" + k + ")").test(format))
+      format = format.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
+      );
+  }
+  return format;
+};
+alert(new Date().format("yyyy-MM-dd hh:mm:ss"));
