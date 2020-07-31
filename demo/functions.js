a0a376f575014bfbacd901a2d37e3c184f6d0742
vip-yy <52729884+yymu@users.noreply.github.com>
2020-07-31 09:03:06 +0800

    Update functions.js

--- demo/functions.js
+++ demo/functions.js
@@ -321,3 +321,33 @@ Date.prototype.format = function(format) {
   return format;
 };
 alert(new Date().format("yyyy-MM-dd hh:mm:ss"));
+
+//toFullScreen：全屏
+function toFullScreen(){
+    let elem = document.body;
+    elem.webkitRequestFullScreen 
+    ? elem.webkitRequestFullScreen()
+    : elem.mozRequestFullScreen
+    ? elem.mozRequestFullScreen()
+    : elem.msRequestFullscreen
+    ? elem.msRequestFullscreen()
+    : elem.requestFullScreen
+    ? elem.requestFullScreen()
+    : alert("浏览器不支持全屏");
+}
+
+//exitFullscreen：退出全屏
+function exitFullscreen(){
+    let elem = parent.document;
+    elem.webkitCancelFullScreen 
+    ? elem.webkitCancelFullScreen()
+    : elem.mozCancelFullScreen
+    ? elem.mozCancelFullScreen()
+    : elem.cancelFullScreen
+    ? elem.cancelFullScreen()
+    : elem.msExitFullscreen
+    ? elem.msExitFullscreen()
+    : elem.exitFullscreen
+    ? elem.exitFullscreen()
+    : alert("切换失败,可尝试Esc退出");
+}
